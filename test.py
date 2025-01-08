9ae17dd687de441568ad100c97ddf526da0a0ec8
ektasri07 <113455213+ektasri07@users.noreply.github.com>
2025-01-08 13:35:35 +0000

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-#Upload PDF files
-st.header("My first Chatbot")
-
-
-with  st.sidebar:
-    st.title("Your Documents")
-    file = st.file_uploader(" Upload a PDf file and start asking questions", type="pdf")
